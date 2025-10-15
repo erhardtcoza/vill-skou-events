473e0dd6ae12c245dd9da5c30a98113456fa7b1b
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-10-15 14:46:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ import { mountWhatsApp } from "./routes/whatsapp.js";
 import { mountCashbar } from "./routes/cashbar.js";
 import { mountItems } from "./routes/items.js";
 import { mountAdminBar } from "./routes/admin_bar.js"; // <-- correct file
+import { mountWhatsAppAdminExtras } from "./routes/whatsapp.js"; // or a new file if you put it elsewhere
+
+
 
 // UI
 import { badgeHTML } from "./ui/badge.js";
@@ -93,6 +96,9 @@ function initWithEnv(env) {
   mountPublicVendors(router);       // /api/public/vendors/*
   mountCashbar(router, env);        // legacy cashbar API
   mountItems(router, env);          // /api/items
+  mountWhatsApp(router);
+  mountWhatsAppAdminExtras(router);
+
 
   /* ------------------- UI ROUTES --------------------- */
   router.add("GET", "/", async () => renderHTML(landingHTML));
